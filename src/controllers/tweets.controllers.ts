d29d6ffb2a3126a2a193dff97e51066c4c80d5c4
hieunm-1158 <nguyen.minh.hieu-b@sun-asterisk.com>
2023-07-26 09:16:59 +0700

    Create tweet

--- src/controllers/tweets.controllers.ts
+++ src/controllers/tweets.controllers.ts
@@ -1,7 +1,15 @@
 import { Response, Request } from 'express';
 import { TweetRequestBody } from '~/models/requests/Tweet.requests';
 import { ParamsDictionary } from 'express-serve-static-core';
+import tweetsService from '~/services/tweets.services';
+import { TWEETS_MESSAGES } from '~/constants/messages';
+import { TokenPayload } from '~/models/requests/User.requests';
 
 export const createTweetController = async (req: Request<ParamsDictionary, any, TweetRequestBody>, res: Response) => {
-  return res.send({ a: 'a' });
+  const { user_id } = req.decoded_authorization as TokenPayload;
+  const result = await tweetsService.createTweet(user_id, req.body);
+  return res.json({
+    message: TWEETS_MESSAGES.CREATE_TWEET_SUCCESS,
+    result
+  });
 };
